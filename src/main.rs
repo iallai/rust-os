ae67be434ec50d4472bbd9acb723e58872d829c6
iallai <iallai.info@gmail.com>
2020-07-21 13:53:32 +0800

    最小系统内核

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,18 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"hello world!";
+
 #[no_mangle] // 不允许改名
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; //raw pointer
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            //每个字符由一个 ASCII byte 和 color byte组成
+        }
+    }
     loop {}
 }
 
